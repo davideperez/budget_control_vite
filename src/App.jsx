1c9e47a1d76cf78f95fe05013b8c22a673f30ff4
David <david.ezequiel.perez@gmail.com>
2023-08-20 17:25:37 -0300

    Add new expense button added, functionality pending.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0)
   const [budgetIsValid, setBudgetIsValid] = useState(false)
   return (
     <div>
+      
       <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         budgetIsValid={budgetIsValid}
         setBudgetIsValid={setBudgetIsValid}
       />
+      
+      {budgetIsValid && (
+        <div className='nuevo-gasto' >
+          <img 
+            src={IconoNuevoGasto} 
+            alt="icono nuevo gasto"
+          />
+        </div>
+      )}
+
     </div>
   )
 }
