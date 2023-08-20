31e0071f09ae70bf7aad4d97dccd432dd221676b
David <david.ezequiel.perez@gmail.com>
2023-08-20 15:59:23 -0300

    Budget input validation implemented to only positive numbers.Error message implemented.

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Mensaje from './Mensaje'
+
 
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+    const [ mensaje, setMensaje ] = useState('')
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault()
+
+        if (!Number ( presupuesto ) || Number ( presupuesto ) < 0) {
+            setMensaje("No es un presupuesto valido")
+        } else {
+            setMensaje("Si es valido.")
+        }
+    }
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario' action="">
+        <form onSubmit={handlePresupuesto} className='formulario' action="">
             <div className='campo'>
                 <label htmlFor="">Definir Presupuesto</label>
                 <input
@@ -11,10 +25,11 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
                     type="text"
                     placeholder='Añáde tu presupuesto'
                     value={presupuesto}
-                    onChange={e => setPresupuesto(e.target.value)}
+                    onChange={ e => setPresupuesto(e.target.value) }
                 />
             </div>
             <input type="submit" value='Añadir' />
+            {mensaje &&  <Mensaje tipo="error" >{mensaje}</Mensaje>} 
         </form>
     </div>
   )
