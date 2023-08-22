bc338f43b21c741324b01f7dfe971707a48d2a2d
David <david.ezequiel.perez@gmail.com>
2023-08-22 10:11:28 -0300

    Now expenses shows on the DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import ListadoGastos from './components/ListadoGastos'
 import Modal from './components/Modal'
 import { generarId } from './helpers'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
@@ -41,13 +42,20 @@ function App() {
       />
       
       {budgetIsValid && (
-        <div className='nuevo-gasto' >
-          <img 
-            src={IconoNuevoGasto} 
-            alt="icono nuevo gasto"
-            onClick={handleNuevoGasto}
-          />
-        </div>
+        <>
+          <main>
+            <ListadoGastos 
+              gastos={gastos}
+            />
+          </main>
+          <div className='nuevo-gasto' >
+            <img 
+              src={IconoNuevoGasto} 
+              alt="icono nuevo gasto"
+              onClick={handleNuevoGasto}
+              />
+          </div>
+        </>
         
       )}
       
