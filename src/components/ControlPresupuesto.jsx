c61afaabddc863308dddd2dcc018c2f6118b4f68
David <david.ezequiel.perez@gmail.com>
2023-08-22 17:17:38 -0300

    Styles for the circle progress bar, and decimals fixing.

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { CircularProgressbar } from 'react-circular-progressbar'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
 
 const ControlPresupuesto = ({gastos, presupuesto }) => {
@@ -12,7 +12,7 @@ const ControlPresupuesto = ({gastos, presupuesto }) => {
       const totalGastado = gastos.reduce( (total, gasto) => gasto.cantidad + total, 0)
       const totalDisponible = presupuesto - totalGastado
 
-      const nuevoPorcentaje = ((presupuesto - totalDisponible) / presupuesto ) * 100
+      const nuevoPorcentaje = Math.ceil(((presupuesto - totalDisponible) / presupuesto ) * 100)
       
       setGastado(totalGastado)
       setDisponible(totalDisponible)
@@ -37,8 +37,14 @@ const ControlPresupuesto = ({gastos, presupuesto }) => {
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
         <div>
-            <CircularProgressbar 
+            <CircularProgressbar
+                styles={buildStyles({
+                    pathColor:"#3B82F6",
+                    trailColor: "#F5F5F5",
+                    textColor:"#3B82F6",
+                })} 
                 value={porcentaje}
+                text={`${porcentaje}% Gastado`}
             />
         </div>
         <div className='contenido-presupuesto'>
